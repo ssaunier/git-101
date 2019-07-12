c387a57970553f7ca0cf6d6de6dde44e954c121b
Sébastien Saunier <seb@saunier.me>
2019-07-12 08:28:28 +0300

    Done - Fetch & display weather info for a city with 2 API calls

--- program.py
+++ program.py
@@ -7,4 +7,12 @@ response = requests.get(url)
 data = response.json()
 woeid = data[0]['woeid']
 
-print(woeid)
+url = f"https://www.metaweather.com/api/location/{woeid}/"
+response = requests.get(url)
+data = response.json()
+
+for day in data['consolidated_weather']:
+    weather = day['weather_state_name']
+    date = day['applicable_date']
+    temp = round(day['max_temp'], 1)
+    print(f"- {date}: {weather} ({temp} C)")
